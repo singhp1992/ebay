db1ee6de6feadfb043d1077fa86384354736ea7d
Preeti <singhp1992@gmail.com>
2018-04-18 09:47:53 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+var Sequelize = require('sequelize')
+var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
+
+
+app.listen(4001, () => console.log('Express API listening on port 4001'))
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
+    next()
+})
+
+//this needs to be manipulated to ad table
+const Advertisement = sequelize.define('advertisement', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    image: Sequelize.STRING, 
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+}, {
+        tableName: 'products',
+        timestamps: false
+    })
+
+//this code will test that it's working 
+Advertisement.findById(1).then(advertisement => console.log(JSON.stringify(advertisement)))
